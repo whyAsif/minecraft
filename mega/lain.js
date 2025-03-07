30d9a24e016fb629ec3d89e859689b1777ffc664
whyAsif <unknownboy398@gmail.com>
2025-03-07 17:51:05 +0600

    lain

--- mega/lain.js
+++ mega/lain.js
@@ -1,6 +1,17 @@
 const mineflayer = require('mineflayer');
 
-// Create the bot
+const express = require('express');
+   const app = express();
+   const port = 3000;
+
+   app.get('/', (req, res) => {
+     res.send('Hello, your app is running 24/7!');
+   });
+
+   app.listen(port, () => {
+     console.log(`Server is running on http://localhost:${port}`);
+   });
+   
 function createBot() {
  bot = mineflayer.createBot({
   host: 'play.themegahive.com',
